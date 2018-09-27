89e544ca210a6cdd3eaee716cbb8be0a21f57587
ghostbuster91 <kghost0@gmail.com>
2018-09-27 20:39:07 +0200

    After starting parking meter it should be started for given plate

--- src/main/kotlin/com/touk/rec/kk/parking/ParkingManager.kt
+++ src/main/kotlin/com/touk/rec/kk/parking/ParkingManager.kt
@@ -2,4 +2,5 @@ package com.touk.rec.kk.parking
 
 interface ParkingManager {
     fun checkMeter(plateNumber: String): Boolean
+    fun startMeter(plateNumber: String)
 }
--- src/test/kotlin/com/touk/rec/kk/parking/DriverTest.kt
+++ src/test/kotlin/com/touk/rec/kk/parking/DriverTest.kt
@@ -2,6 +2,7 @@ package com.touk.rec.kk.parking
 
 import assertk.assert
 import assertk.assertions.isFalse
+import assertk.assertions.isTrue
 import org.junit.Test
 
 class DriverTest {
@@ -11,11 +12,23 @@ class DriverTest {
         val parkingManager = SimpleParkingManager()
         assert(parkingManager.checkMeter("wn1111")).isFalse()
     }
+
+    @Test
+    fun `after starting parking meter it should be started for given plate`() {
+        val parkingManager = SimpleParkingManager()
+        parkingManager.startMeter("wn1111")
+        assert(parkingManager.checkMeter("wn1111")).isTrue()
+    }
 }
 
 class SimpleParkingManager : ParkingManager {
+    private var started = false
+
+    override fun startMeter(plateNumber: String) {
+        started = true
+    }
 
     override fun checkMeter(plateNumber: String): Boolean {
-        return false
+        return started
     }
 }
