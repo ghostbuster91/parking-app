70a016d83c04f4918ef6043323884780c0e06fa5
ghostbuster91 <kghost0@gmail.com>
2018-09-28 20:25:49 +0200

    [Refactor] Extract paymentCalculator to production code

--- src/test/kotlin/com/touk/rec/kk/parking/PaymentCalculatorTest.kt
+++ src/test/kotlin/com/touk/rec/kk/parking/PaymentCalculatorTest.kt
@@ -9,7 +9,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import java.math.BigDecimal
-import java.time.Duration
 import java.time.LocalDateTime
 
 @RunWith(Parameterized::class)
@@ -74,40 +73,5 @@ class PaymentCalculatorTest(
         private fun bigDecimal(number: Double): BigDecimal {
             return BigDecimal.valueOf(number).setScale(2)
         }
-    }
-}
-
-class PaymentCalculator(
-        private val repository: ParkingMeterRepository,
-        private val currentTimeProvider: CurrentTimeProvider
-) {
-    fun calculateTotal(plateNumber: String): BigDecimal {
-        val meterRecord = repository.find(plateNumber) ?: return BigDecimal.ZERO
-        val upperTimeLimit = (meterRecord.endDate ?: currentTimeProvider.getCurrentLocalDateTime()).plusMinutes(59)
-        val totalHours = Duration.between(meterRecord.startDate, upperTimeLimit).toHours().toInt()
-        return (1..totalHours)
-                .fold(listOf<BigDecimal>()) { acc, item ->
-                    acc + when (meterRecord.driverType) {
-                        DriverType.REGULAR -> calculatePriceForRegular(item, acc)
-                        DriverType.DISABLED -> calculatePriceForDisabled(item, acc)
-                    }
-                }
-                .reduce { first, second -> first + second }
-    }
-
-    private fun calculatePriceForDisabled(item: Int, acc: List<BigDecimal>): BigDecimal {
-        return when {
-            item <= 1 -> BigDecimal.valueOf(0)
-            item in 2..2 -> BigDecimal.valueOf(2)
-            else -> acc.last().multiply(BigDecimal.valueOf(1.2))
-        }.setScale(2)
-    }
-
-    private fun calculatePriceForRegular(item: Int, acc: List<BigDecimal>): BigDecimal {
-        return when {
-            item <= 1 -> BigDecimal.valueOf(1)
-            item in 2..2 -> BigDecimal.valueOf(2)
-            else -> acc.last().multiply(BigDecimal.valueOf(1.5))
-        }.setScale(2)
     }
 }
