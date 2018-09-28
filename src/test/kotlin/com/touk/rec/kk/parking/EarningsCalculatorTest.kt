587dcf74cf15360e4b02f2636d64b5f670ae2ff9
ghostbuster91 <kghost0@gmail.com>
2018-09-28 21:37:34 +0200

    [Refactor] Move earningsCalculator to production code

--- src/test/kotlin/com/touk/rec/kk/parking/EarningsCalculatorTest.kt
+++ src/test/kotlin/com/touk/rec/kk/parking/EarningsCalculatorTest.kt
@@ -38,19 +38,3 @@ class EarningsCalculatorTest {
         assert(calculator.getEarnings(LocalDate.MIN)).isEqualTo(BigDecimal.TEN + BigDecimal.valueOf(6.66))
     }
 }
-
-class EarningsCalculator(
-        private val repository: ParkingMeterRepository,
-        private val paymentCalculator: PaymentCalculator
-) {
-    fun getEarnings(date: LocalDate): BigDecimal {
-        val completedRecords = repository.getCompletedRecords(date)
-        return completedRecords
-                .map { paymentCalculator.calculateTotal(it) }
-                .fold(BigDecimal.ZERO) {acc, item -> acc + item }
-    }
-
-    interface ParkingMeterRepository {
-        fun getCompletedRecords(date: LocalDate): List<ParkingMeterRecord>
-    }
-}
