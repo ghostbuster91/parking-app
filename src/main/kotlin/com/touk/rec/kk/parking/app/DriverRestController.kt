f17aaa26709998a1b86620000d2164d4fbd81ec6
ghostbuster91 <kghost0@gmail.com>
2018-09-29 10:56:59 +0200

    Should return return total payment for given plateNumber

--- src/main/kotlin/com/touk/rec/kk/parking/app/DriverRestController.kt
+++ src/main/kotlin/com/touk/rec/kk/parking/app/DriverRestController.kt
@@ -2,13 +2,22 @@ package com.touk.rec.kk.parking.app
 
 import com.touk.rec.kk.parking.domain.DriverType
 import com.touk.rec.kk.parking.domain.ParkingMeter
+import com.touk.rec.kk.parking.domain.ParkingMeterRepository
+import com.touk.rec.kk.parking.domain.PaymentCalculator
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.lang.RuntimeException
+import java.math.BigDecimal
 import java.net.URI
 
 @RestController
 @RequestMapping("/driver")
-class DriverRestController(private val parkingMeter: ParkingMeter) {
+class DriverRestController(
+        private val parkingMeter: ParkingMeter,
+        private val parkingMeterRepository: ParkingMeterRepository,
+        private val paymentCalculator: PaymentCalculator
+) {
 
     @PostMapping("/startMeter")
     fun startMeter(@RequestBody request: Request): ResponseEntity<URI> {
@@ -22,10 +31,22 @@ class DriverRestController(private val parkingMeter: ParkingMeter) {
         return ResponseEntity.noContent().build()
     }
 
+    @GetMapping("/{plateNumber}")
+    fun getBilling(@PathVariable("plateNumber") plateNumber: String): BillingResponse {
+        val meterRecord = parkingMeterRepository.find(plateNumber) ?: throw PlateNumberNotFound()
+        return paymentCalculator.calculateTotal(meterRecord)
+                .let(::BillingResponse)
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    class PlateNumberNotFound : RuntimeException()
+
     data class Request(
             val plateNumber: String,
             val driverType: DriverType
     )
+
+    data class BillingResponse(val total: BigDecimal)
 
     @ExceptionHandler
     fun errorHandler(ex: IllegalArgumentException): ResponseEntity<Nothing> {
--- src/test/kotlin/com/touk/rec/kk/parking/app/DriverRestControllerTest.kt
+++ src/test/kotlin/com/touk/rec/kk/parking/app/DriverRestControllerTest.kt
@@ -4,8 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
-import com.touk.rec.kk.parking.domain.DriverType
-import com.touk.rec.kk.parking.domain.ParkingMeter
+import com.touk.rec.kk.parking.domain.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -15,14 +14,20 @@ import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.ResultActions
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.header
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import java.math.BigDecimal
+import java.time.LocalDateTime
 
 @RunWith(SpringRunner::class)
 @WebMvcTest(DriverRestController::class)
 class DriverRestControllerTest {
+
+    @MockBean
+    private lateinit var paymentCalculator: PaymentCalculator
+
+    @MockBean
+    private lateinit var parkingMeterRepository: ParkingMeterRepository
 
     @MockBean
     private lateinit var parkingMeter: ParkingMeter
@@ -56,6 +61,22 @@ class DriverRestControllerTest {
         whenever(parkingMeter.stopMeter(any())).thenThrow(IllegalArgumentException())
         mvc.perform(put("/driver/$PLATE_ONE/stopMeter"))
                 .andExpect(status().isBadRequest)
+    }
+
+    @Test
+    fun `should return 404 when trying to get billing for not registered plate numbers`() {
+        mvc.perform(get("/driver/$PLATE_ONE"))
+                .andExpect(status().isNotFound)
+    }
+
+    @Test
+    fun `should return return total payment for given plateNumber`() {
+        val meterRecord = ParkingMeterRecord(PLATE_ONE, LocalDateTime.MIN, LocalDateTime.MIN, DISABLED_DRIVER)
+        whenever(parkingMeterRepository.find(any())).thenReturn(meterRecord)
+        whenever(paymentCalculator.calculateTotal(meterRecord)).thenReturn(BigDecimal.valueOf(22))
+        mvc.perform(get("/driver/$PLATE_ONE"))
+                .andExpect(status().isOk)
+                .andExpect(content().json(ObjectMapper().writeValueAsString(DriverRestController.BillingResponse(BigDecimal.valueOf(22)))))
     }
 
     private fun startMeter(plateNumber: String, driverType: DriverType): ResultActions {
